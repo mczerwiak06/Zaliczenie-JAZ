3441f1a60024aa7c91c9ea67134d942f96da8310
Michał Czerwiak <michal.czerwiak@gmail.com>
2021-07-03 16:53:43 +0200

    Added models and service classes

--- jaz-s21356-nbp/src/main/java/pjatk/jaz/jazs21356nbp/service/NbpService.java
+++ jaz-s21356-nbp/src/main/java/pjatk/jaz/jazs21356nbp/service/NbpService.java
@@ -2,6 +2,15 @@ package pjatk.jaz.jazs21356nbp.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import pjatk.jaz.jazs21356nbp.model.Rate;
+import pjatk.jaz.jazs21356nbp.model.Root;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class NbpService {
@@ -10,5 +19,25 @@ public class NbpService {
 
     public NbpService(RestTemplate restTemplate){
         this.restTemplate = restTemplate;
+    }
+
+    public Double getAvarageRate(String currency, String sDateFrom, String sDateTo) throws ParseException {
+        String url = "http://api.nbp.pl/api/exchangerates/rates/a/" + currency + sDateFrom + sDateTo;
+        Root root = restTemplate.getForObject(url, Root.class);
+        List<Rate> tableOfCurrencyValues = root.getRates();
+        //SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+        //Date dateFrom = format1.parse(sDateFrom);
+        //Date dateTo = format1.parse(sDateTo);
+         //long daysBetween = ChronoUnit.DAYS.between(dateFrom, dateTo);
+        LocalDate dateBefore = LocalDate.parse(sDateFrom);
+        LocalDate dateAfter = LocalDate.parse(sDateTo);
+
+        long daysCount = ChronoUnit.DAYS.between(dateBefore, dateAfter);
+
+        double sumaricRate = 0.0;
+        for(Rate currencies :tableOfCurrencyValues){
+            sumaricRate += currencies.getMid();
+        }
+        return sumaricRate/daysCount;
     }
 }

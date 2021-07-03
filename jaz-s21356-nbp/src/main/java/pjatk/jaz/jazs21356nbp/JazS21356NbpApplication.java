faa948875fa980d369ead0d06fffa79cff9de1a9
Michał Czerwiak <michal.czerwiak@gmail.com>
2021-07-03 16:11:15 +0200

    Started project

---
+++ jaz-s21356-nbp/src/main/java/pjatk/jaz/jazs21356nbp/JazS21356NbpApplication.java
@@ -0,0 +1,13 @@
+package pjatk.jaz.jazs21356nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21356NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS21356NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s21356-nbp/src/test/java/pjatk/jaz/jazs21356nbp/JazS21356NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.jaz.jazs21356nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21356NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
